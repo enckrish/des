0c32848e247dee69b633189d2ecf7334064ba6cb
enckrish <ksengupta29.11@gmail.com>
2025-12-02 14:08:05 +0530

    correct tests

--- tests/DESTests.cpp
+++ tests/DESTests.cpp
@@ -32,20 +32,30 @@ TEST_CASE("DES process()<->Engine Matching", "[DESTests]") {
      uint_fast64_t key = rand64();
     const DES::Engine engine(key);
 
+    // Converting the key to BE-format to make it comparable to DES::Engine's,
+    // which converts it internally
 #if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
     key = __builtin_bswap64(key);
 #endif
 
      uint_fast64_t data = rand64();
 
+    // To reduce data's endianness changes, we are assuming that data is
+    // already in BE, but since DES::Engine will change the endianness of data
+    // when it's on a LE machine, we will do one conversion before Engine::encrypt
+    const auto enc_pr = DES::process(data, key, true);
+
+
+    auto dec_pr = DES::process(enc_pr, key, false);
+    // Changing dec_pr's endianness to match with Engine's
 #if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
-    data = __builtin_bswap64(data);
+    dec_pr = __builtin_bswap64(dec_pr);
 #endif
 
-    const auto enc_pr = DES::process(data, key, true);
-    auto dec_pr = DES::process(enc_pr, key, false);
-
     SECTION("Encryption") {
+#if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
+        data = __builtin_bswap64(data);
+#endif
         const auto enc_en = engine.encrypt(data);
         INFO("Encrypt Process:\t" << std::bitset<64>(enc_pr));
         INFO("Encrypt Engine:\t" << std::bitset<64>(enc_en));
@@ -53,10 +63,6 @@ TEST_CASE("DES process()<->Engine Matching", "[DESTests]") {
     }
 
     SECTION("Decryption") {
-#if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
-        dec_pr = __builtin_bswap64(dec_pr);
-#endif
-
         const auto dec_en = engine.decrypt(enc_pr);
         INFO("Decrypt Process:\t" << std::bitset<64>(dec_pr));
         INFO("Decrypt Dec:\t" << std::bitset<64>(dec_en));
